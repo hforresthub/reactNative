0d57a5bc9f7b12366784839c9c25b6ba220a07e6
Hal <hgrforrest@gmail.com>
2022-02-15 15:02:26 -0500

    testing buttons

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { touchProps } from 'react-native-web/dist/cjs/modules/forwardedProps';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-	  <Testing name="Test1" />
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [count, setCount] = useState(0)
+
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your app!</Text>
+			<Button 
+				onPress={() => setCount(count + 1)}
+				title="Click!"
+			/>
+			<Text>Clicked {count} times!</Text>
+			<Testing name="Test1" />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
 
 const Testing = (props) => {
